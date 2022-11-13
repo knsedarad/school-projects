8754e81856cc99c16ba1acded4ca67d4864dee44
Arad Shayan <117934312+knsedarad@users.noreply.github.com>
2022-11-13 20:11:03 +0330

    Update not finished.java

--- java/1st HW's/not finished.java
+++ java/1st HW's/not finished.java
@@ -3,25 +3,16 @@ public class worldcup {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-        if (a%4==2) {
+        if (a%4==0) {
             System.out.println("u have world cup in "+a);
         }
-        else {
-            a=a+1;
-        }
-        if (a%4==2) {
+        else if (a%4==1) {
             System.out.println("world cup on "+a);
         }
-        else {
-            a=a+2;
-        }
-        if (a%4==2) {
+        else if (a%4==2) {
             System.out.println("worldcup in "+a);
         }
-        else {
-            a=a+3;
-        }
-        if (a%4==2) {
+        else if (a%4==3) {
             System.out.println("world cup in "+a);
         }
         else {
